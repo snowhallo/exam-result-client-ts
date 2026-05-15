bfb7232306e6714cef7ba0f88eae6c17c466d2bf
snowhallo <snowhallo99@gmail.com>
2026-05-15 22:08:04 +0700

    fix filter all provinces

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -6,6 +6,6 @@ export const searchByName = (schools: School[], name: string) => {
 }
 
 export const searchByProvince = (schools: School[], province: string) => {
-    if (!province.trim()) return schools;
+    if (!province.trim() || province.trim() === "All") return schools;
     return schools.filter((school) => school.province.toLowerCase().includes(province.toLowerCase()));
 }
